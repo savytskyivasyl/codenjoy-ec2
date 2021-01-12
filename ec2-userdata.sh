a5327d878e85e83eb7d246662add197430fc0ce5
vasyl.savytskyi <savytskyi.vasyl@tapad.com>
2021-01-12 13:59:25 +0100

    add ec2 userdata for server launch

---
+++ ec2-userdata.sh
@@ -0,0 +1,50 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+sudo yum update -y
+
+#install java:11
+sudo amazon-linux-extras install java-openjdk11 -y
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.7.10-4.amzn2.0.1.x86_64
+
+#install maven:3.5.4
+wget http://www-us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+tar -zxvf apache-maven-3.5.4-bin.tar.gz
+sudo mv apache-maven-3.5.4 /opt
+sudo chown -R root:root /opt/apache-maven-3.5.4
+sudo ln -s /opt/apache-maven-3.5.4 /opt/apache-maven
+echo 'export PATH=$PATH:/opt/apache-maven/bin' | sudo tee -a /etc/profile
+source /etc/profile
+
+#install git
+sudo yum install git -y
+
+#clone codenjoy repo
+git clone https://github.com/fausternet/codenjoy.git
+
+#lanch codenjoy server
+cd codenjoy/CodingDojo/games/engine/
+mvn clean install -N -DskipTests=true
+cd ..
+mvn clean install -N -DskipTests=true
+cd bomberman
+mvn clean install -N -DskipTests=true
+cd ../../server
+nohup mvn clean spring-boot:run -DMAVEN_OPTS=-Xmx1024m -Dmaven.test.skip=true -Dspring.profiles.active=sqlite,bomberman,debug -Dcontext=/codenjoy-contest -Dserver.port=8080 -Pbomberman &
