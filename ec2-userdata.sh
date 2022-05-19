90179684493e162cc8e00167f29f9b72daa5e2e8
Vasyl Savytskyi <vasylsavytskyi@fb.com>
2022-05-19 16:12:54 +0100

    update script

--- ec2-userdata.sh
+++ ec2-userdata.sh
@@ -22,10 +22,10 @@ sudo yum update -y
 
 #install java:11
 sudo amazon-linux-extras install java-openjdk11 -y
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.7.10-4.amzn2.0.1.x86_64
+export JAVA_HOME=$(echo /usr/lib/jvm/)$(echo $(ls /usr/lib/jvm | grep "java-11-openjdk*"))
 
 #install maven:3.5.4
-wget http://www-us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
+wget https://archive.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
 tar -zxvf apache-maven-3.5.4-bin.tar.gz
 sudo mv apache-maven-3.5.4 /opt
 sudo chown -R root:root /opt/apache-maven-3.5.4
@@ -39,6 +39,9 @@ sudo yum install git -y
 #clone codenjoy repo
 git clone https://github.com/codenjoyme/codenjoy.git
 
+#checkout working commit
+git checkout 7c8e2ceb2d74dcb18a344ca32bcacf5bd4a2bb39
+
 #lanch codenjoy server
 cd codenjoy/CodingDojo/games/engine/
 mvn clean install -N -DskipTests=true
@@ -47,4 +50,4 @@ mvn clean install -N -DskipTests=true
 cd bomberman
 mvn clean install -N -DskipTests=true
 cd ../../server
-nohup mvn clean spring-boot:run -DMAVEN_OPTS=-Xmx1024m -Dmaven.test.skip=true -Dspring.profiles.active=sqlite,bomberman,debug -Dcontext=/codenjoy-contest -Dserver.port=8080 -Pbomberman &
+nohup mvn clean spring-boot:run -DMAVEN_OPTS=-Xmx1024m -Dmaven.test.skip=true -Dspring.profiles.active=sqlite,bomberman,debug -Dcontext=/codenjoy-contest -Dserver.port=80 -Pbomberman &
